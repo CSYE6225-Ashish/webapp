6f97c64db905c93eee413c79ae6eb0e2ba7bcf3b
Ashish Babish Gangaramani <gangaramani.a@northeastern.edu>
2025-01-28 21:04:10 -0500

    Connected to mysql database using sequelize

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const sequelize = require('./config/database'); 
 
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+sequelize.authenticate().then(() => {
+    console.log('Connection to database has been established successfully.');
+ }).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+ });
+ 
 
 app.listen(port, () => {
   console.log(`App listening on the port ${port}`);
 });
+
