f1b3bc752d7446106d55949c3f01c469fdb17b91
Ashish Babish Gangaramani <gangaramani.a@northeastern.edu>
2025-03-25 22:44:28 -0400

    Testing metrics and logs

---
+++ utils/logger.js
@@ -0,0 +1,26 @@
+const winston = require('winston');
+const path = require('path');
+const fs = require('fs');
+
+const logDir = path.join(__dirname, '../logs');
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
+}
+
+const logger = winston.createLogger({
+    level: 'info',  // Default log level
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level}]: ${message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console({ level: 'info' }),
+        new winston.transports.File({ filename: path.join(logDir, 'info.log'), level: 'info' }),
+        new winston.transports.File({ filename: path.join(logDir, 'warn.log'), level: 'warn' }),
+        new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' })
+    ],
+});
+
+module.exports = logger;
