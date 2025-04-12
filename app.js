93a8e1340c24761aa44a48ba5024374f0e43552b
Ashish Babish Gangaramani <gangaramani.a@northeastern.edu>
2025-04-11 22:48:43 -0400

    Ami creationwith instance refresh (#67)
    * Testing AMI creation with Instance refresh steps
    
    * Changing refresh time in instance refresh

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ sequelize.sync().then(
 
 
 app.use('/healthz', middleware, healthCheckRoutes);
-app.use('/healthz_refresh', middleware, healthCheckRoutes);
+app.use('/healthz2', middleware, healthCheckRoutes);
 app.use('/v1/file',fileUploadRoutes);
 app.use('/*',middleware,notAllowedRoutes);
 
